af1897a074a56b3d83f47b5e2e3093e800e1e82f
s0punk <simonoliviervaill@gmail.com>
2024-08-26 22:05:27 -0400

    Add contact config.

--- lib/flutter_email_contact.dart
+++ lib/flutter_email_contact.dart
@@ -3,4 +3,5 @@ library flutter_email_contact;
 export 'models/contact_email_options.dart';
 export 'models/email.dart';
 export 'models/message.dart';
+export 'models/contact_config.dart';
 export 'views/contact_form.dart';
---
+++ lib/models/contact_config.dart
@@ -0,0 +1,16 @@
+import 'dart:convert';
+
+import 'package:flutter_email_contact/flutter_email_contact.dart';
+import 'package:flutter_fit_utils/model/model.dart';
+import 'package:flutter_fit_utils_config/remote_config.dart';
+
+class ContactConfig extends RemoteConfig {
+  static const String _emailConfigKey = "email_contact";
+
+  late ContactEmailOptions emailOptions;
+
+  @override
+  void read() {
+    emailOptions = ContactEmailOptions.fromModel(Model.fromJson(jsonDecode(appConfig.getString(_emailConfigKey)) as Map<String, dynamic>));
+  }
+}
